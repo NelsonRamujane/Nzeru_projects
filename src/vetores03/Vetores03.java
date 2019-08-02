74cc8558f3a7a1c7b594352a8b86fa97d5ca5e80
Nelson Ramujane <ramujane89@gmail.com>
2019-08-02 18:25:43 +0100

    Projeto criado

---
+++ src/vetores03/Vetores03.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vetores03;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Hp
+ */
+public class Vetores03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        String []nomes= new String[]{"nelsn","maneira","ramujane","Ana Maria","Manjor"};
+        
+        for(int i=0;i<nomes.length;i++){
+          System.out.println(nomes[i]);
+        }
+        
+        //ou de forma mais facil
+        for (String i:nomes){
+        System.out.println(i);
+        }
+            
+            
+    }
+    
+}
